588e73994cd4344bc5c490acd07d477e48252c04
Chandra Lingam <20747492+ChandraLingam@users.noreply.github.com>
2022-05-25 15:26:26 -0400

    Embedded Comma

--- TestProject/QuestionAnswers.cs
+++ TestProject/QuestionAnswers.cs
@@ -7,9 +7,18 @@ namespace TestProject
     [TestClass]
     public class QuestionAnswers
     {
+        /*
+         * Objective: Remove Embedded Comma
+         * Question and pattern by Minhchau:
+            https://www.udemy.com/course/learning-regular-expression-with-net/learn/#questions/17551850/
+        */
+
         [TestMethod]
         public void RemoveEmbeddedComma()
         {
+            // rewrote the pattern
+            //  using multiline and make sure double quotes
+            //  are escaped properly
             string pattern = @"(?m)(""[^"",]+),([^""]+"")";
 
             Console.WriteLine(pattern);
@@ -20,9 +29,10 @@ namespace TestProject
 
             var fileContents = System.IO.File.ReadAllText(directory + @"\" + fileName);
 
-            Console.WriteLine("***");
+            Console.WriteLine("*** Original Content ***");
             Console.WriteLine(fileContents);
             fileContents = Regex.Replace(fileContents, pattern, sub);
+            Console.WriteLine("*** Modified Content ***");
             Console.WriteLine(fileContents);
         }
     }
