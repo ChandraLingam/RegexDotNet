c6ad2bb62082fe2db9a612043ee5657f08c5c726
Chandra Lingam <20747492+ChandraLingam@users.noreply.github.com>
2022-05-25 15:13:47 -0400

    ExampleRemoveEmbeddedComma

---
+++ TestProject/QuestionAnswers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text.RegularExpressions;
+
+namespace TestProject
+{
+    [TestClass]
+    public class QuestionAnswers
+    {
+        [TestMethod]
+        public void RemoveEmbeddedComma()
+        {
+            string pattern = @"(?m)(""[^"",]+),([^""]+"")";
+
+            Console.WriteLine(pattern);
+
+            string sub = "$1$2";
+            string directory = @"C:\Git\RegexDotNet\Data\QuestionAnswerData";
+            string fileName = @"embeddedQuotes.csv";
+
+            var fileContents = System.IO.File.ReadAllText(directory + @"\" + fileName);
+
+            Console.WriteLine("***");
+            Console.WriteLine(fileContents);
+            fileContents = Regex.Replace(fileContents, pattern, sub);
+            Console.WriteLine(fileContents);
+        }
+    }
+}
